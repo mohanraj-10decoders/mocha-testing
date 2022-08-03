794ceed404ddf7dc21fe7cf7c36995ea8a00cf74
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-08-03 17:33:37 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        Mocha
       </header>
     </div>
   );
